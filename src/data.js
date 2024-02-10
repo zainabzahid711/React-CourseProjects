ebd62ee2d523cbd80729c5ceba5209fa30efb6db
zainabzahid711 <zainabzahid711@gmail.com>
2024-02-10 21:09:56 +0500

    enter dummy data in app component

--- src/data.js
+++ src/data.js
@@ -30,3 +30,29 @@ export const dataDescription = [
       "React-mananged data which, when changed courses the component to re-render & the UI to update",
   },
 ];
+export const EXAMPLES = {
+  components: {
+    title: "Components",
+    description:
+      "Components are the building blocks of a React application. A component is nothing more than a piece of UI.",
+    code: "function welcome() { return <h1>Hello World!</h1>; }",
+  },
+  JSX: {
+    title: "JSX",
+    description:
+      "JSX is a syntax extension for JavaScript which allows you to write HTML-like elements in your JavaScript code. It is transpiled to regular JavaScript at build time.",
+    code: "const element = <h1>Hello, JSX!</h1>;",
+  },
+  Props: {
+    title: "Props",
+    description:
+      "Props (short for properties) are a way of passing data from parent to child components in React. They are immutable and are used to customize components.",
+    code: "function Greeting(props) { return <h1>Hello, {props.name}!</h1>; }",
+  },
+  State: {
+    title: "State",
+    description:
+      "State is a data structure in React that is used to keep track of component data that may change over time. It allows components to manage their own data.",
+    code: "class Counter extends React.Component { constructor(props) { super(props);",
+  },
+};
