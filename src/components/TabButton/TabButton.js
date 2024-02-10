1e2b6b1ff2d26229f0fbc446df0865967de3f27e
zainabzahid711 <zainabzahid711@gmail.com>
2024-02-10 21:10:18 +0500

    modified

--- src/components/TabButton/TabButton.js
+++ src/components/TabButton/TabButton.js
@@ -2,10 +2,6 @@ import React from "react";
 import style from "./TabButton.module.css";
 
 function TabButton({ children, onSelect }) {
-  function handleClick() {
-    console.log("helo react");
-  }
-
   return (
     <li className={style.btnLists}>
       <button className={style.tabButton} onClick={onSelect}>
